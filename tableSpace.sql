b2d989d016acd7823caa4775205f35e33016023e
Alessandro <alessandroempire@gmail.com>
2014-10-29 01:17:01 -0430

    tableSpace

--- tableSpace.sql
+++ tableSpace.sql
@@ -6,14 +6,15 @@ create tablespace "CAT1"
 datafile 'tablespace/rosetta/rosetta/cat1.dbf' SIZE 1 MB
 AUTOEXTEND OFF
 PCTFREE 0
---BLOCKSIZE
---EXTENT_MANAGEMENT_CLAUSE UNIFORM SIZE 0.7
---SEGMENT_MANAGMENT_CLAUSE { AUTO | MANUAL }
+PCTUSED 80
+--BLOCKSIZE --no se....
+--EXTENT_MANAGEMENT_CLAUSE UNIFORM SIZE 0.7 (holgura)
+--SEGMENT_MANAGMENT_CLAUSE { AUTO | MANUAL } --no se...
 
 create tablespace "CAT2"
 datafile 'tablespace/rosetta/rosetta/cat2.dbf' size 1245 MB
 --AUTOEXTEND OFF
---PCTUSED
+--PCTUSED 
 --PCTFREE 
 --BLOCKSIZE
 --EXTENT_MANAGEMENT_CLAUSE LOCAL 
