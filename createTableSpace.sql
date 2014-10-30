d8c97e4f9de39d8aca819d0eab7cf467e44e1002
Alessandro <alessandroempire@gmail.com>
2014-10-30 07:32:01 -0430

    cambiando el createTableSpace, agregando storage a create y arregloando drop

--- createTableSpace.sql
+++ createTableSpace.sql
@@ -3,7 +3,7 @@ spool &FILE
 
 ----- creamos el tableSpace para nation y regions
 create tablespace "CAT1"
-datafile '/tablespace/rosetta/rosetta/cat1.dbf' SIZE 1M
+datafile '/tablespace/rosetta/rosetta/catdb1.dbf' SIZE 1M
 AUTOEXTEND OFF
 LOGGING ONLINE PERMANENT
 EXTENT MANAGEMENT LOCAL UNIFORM SIZE 128 K 
@@ -12,7 +12,7 @@ SEGMENT SPACE MANAGEMENT MANUAL;
 
 --la suma de todas las tablas (menos nations y regions es 1079 MB)
 create tablespace "CAT2"
-datafile '/tablespace/rosetta/rosetta/cat2.dbf' size 2 G
+datafile '/tablespace/rosetta/rosetta/catdb2.dbf' size 2 G
 AUTOEXTEND ON NEXT 4M MAXSIZE 3 G
 LOGGING ONLINE PERMANENT
 EXTENT MANAGEMENT LOCAL
